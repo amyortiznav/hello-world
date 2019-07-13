d87ca6fe7337f0dbf1289ea0995db0bed1d85cf6
amyortiznav <52843738+amyortiznav@users.noreply.github.com>
2019-07-12 23:10:12 -0700

    Add files via upload

---
+++ ToDoList.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 22:07:08 2019
+
+@author: amyortiz
+"""
+name = input("What's your name?")
+
+toDoList = []
+
+toDoList.append(input ("Okay" + name + " what's on your to do list?"))
+
+
+option = input("Is that it," + name + "? ")
+
+while option.lower() == "no":
+    toDoList.append(input("What else we doing?"))
+    optionRepeat = input("is that it now? ")
+    option=optionRepeat
+print(toDoList)
+
+
+    
+
+
+
